95f34c58bdbb355c04aee69d50439778359c935d
bin0 <binariaesidus@gmail.com>
2017-05-24 21:25:18 -0400

    general idea to get going

---
+++ src/Menu.java
@@ -0,0 +1,18 @@
+
+public class Menu {
+
+    // main menu class / program
+    public static void main(String[] args) {
+
+        System.out.println("BASIC Computer Games converted into Java");
+        System.out.println("Taken from http://www.atariarchives.org/basicgames/");
+        System.out.println();
+        System.out.println("Select Game: ");
+        // do loop with the selection of games in alphabetical order
+        // number choices, last number or q or quit or exit to exit
+        // each game will be their own class, game called upon Game.gamemethod()
+        System.out.println("1. some game");
+
+
+    }
+}
